2bb103c6d0211db4e6bd040951a9cfdc21fb4121
freestyletear <freestyletear@gmail.com>
2021-08-01 11:11:05 +0300

    modify code

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Link Collaps
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+  navLinks.style.right = "0";
+}
+
+function hideMenu() {
+  navLinks.style.right = "-300px";
+}
+
+// $(document).ready(function () {
+//   $(".navbar-nav li a").click(function (event) {
+//     $(".navbar-collapse").collapse("hide");
+//   });
+// });
